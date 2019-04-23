4941bb4f07e84511c380cfd723b98af957e6ed88
Marcin <31897007+marcin-wski@users.noreply.github.com>
2019-04-22 18:16:25 -0700

    Create class-work-13b.js

---
+++ COMP484 - Web Engineering/class-work-13b.js
@@ -0,0 +1,12 @@
+currentNode = document.getElementById( "bigheading" );
+currentNode.setAttribute( "class", "highlighted" );
+currentNode.getAttribute( "id" );
+currentNode.removeChild( oldNode );
+currentNode.parentNode
+document.body
+currentNode.replaceChild( newChild, currentChild );
+currentNode.parentNode.replaceChild( newNode, currentNode );
+document.createTextNode( text )
+document.createElement( "p" )
+newNode.appendChild( document.createTextNode( text ) );
+currentNode.insertBefore( newNode, existingNode );
