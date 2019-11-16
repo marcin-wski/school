9cdafa2419b746b616c266d689e164f1e8738f3c
Marcin <31897007+marcin-wski@users.noreply.github.com>
2019-11-16 02:03:40 -0800

    fixing

--- CIT480 - Senior Design/Terraform/acm_cert.tf
+++ CIT480 - Senior Design/Terraform/acm_cert.tf
@@ -1,31 +1,22 @@
-provider "aws" {
-  alias = "acm"
-  region = "us-west-2"
-  version = "2.24"
+resource "aws_acm_certificate" "main" {
+  domain_name = "downtimerus.net"
+  subject_alternative_names = ["*.downtimerus.net"]
+  validation_method = "DNS"
 }
 
-resource "aws_acm_certificate" "default" {
-  provider = "aws.acm"
-  domain_name = "downtimerus.net"
-  subject_alternative_names = "*.downtimerus.net"
-  validation_method = "DNS"
-  lifecycle {
-    create_before_destroy = true
-  }
+data "aws_route53_zone" "validation" {
+  name = "downtimerus.net."
 }
 
 resource "aws_route53_record" "validation" {
-  zone_id = "${aws_route53_zone.public_zone.zone_id}"
-  name = "${aws_acm_certificate.default.domain_validation_options.0.resource_record_name}"
-  type = "${aws_acm_certificate.default.domain_validation_options.0.resource_record_type}"
-  records = ["${aws_acm_certificate.default.domain_validation_options.0.resource_record_value}"]
-  ttl = "300"
+  name = "${aws_acm_certificate.main.domain_validation_options[0].resource_record_name}"
+  type = "${aws_acm_certificate.main.domain_validation_options[0].resource_record_type}"
+  zone_id = "${data.aws_route53_zone.validation.zone_id}"
+  records = ["${aws_acm_certificate.main.domain_validation_options[0].resource_record_value}"]
+  ttl = 300
 }
 
-resource "aws_acm_certificate_validation" "default" {
-  provider = "aws.acm"
-  certificate_arn = "${aws_acm_certificate.default.arn}"
-  validation_record_fqdns = [
-    "${aws_route53_record.validation.fqdn}",
-  ]
+resource "aws_acm_certificate_validation" "main" {
+  certificate_arn = "${aws_acm_certificate.main.arn}"
+  validation_record_fqdns = ["${aws_route53_record.validation.*.fqdn}"]
 }
